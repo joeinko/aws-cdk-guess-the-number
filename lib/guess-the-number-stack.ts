9bad0a6d3903fe041b3fb065ba8cb754792671ad
Martin Koprna <joeblackofficial@gmail.com>
2026-02-23 03:18:14 +0100

    ref: extract lambda props

--- lib/guess-the-number-stack.ts
+++ lib/guess-the-number-stack.ts
@@ -1,10 +1,12 @@
 import * as cdk from 'aws-cdk-lib/core';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
-import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
-import path from "node:path";
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import path from 'node:path';
+
+const LAMBDA_DIR = path.join(__dirname, '../lambda');
 
 export class GuessTheNumberStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -14,31 +16,25 @@ export class GuessTheNumberStack extends cdk.Stack {
 
     const dbTable = new dynamodb.Table(this, GAME_TABLE_NAME, {
       tableName: GAME_TABLE_NAME,
-      partitionKey: { 
-        name: 'id', 
-        type: dynamodb.AttributeType.STRING 
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING
       },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    const startGameLambda = new NodejsFunction(this, 'StartGameFunction', {
-      runtime: lambda.Runtime.NODEJS_22_X,
-      entry: path.join(__dirname, "../lambda/startGame.ts"),
+    const lambdaProps = (filename: string): NodejsFunctionProps => ({
+      runtime: Runtime.NODEJS_22_X,
+      entry: path.join(LAMBDA_DIR, filename),
       handler: 'handler',
       environment: {
-        TABLE_NAME: dbTable.tableName,
+        TABLE_NAME: dbTable.tableName
       },
     });
 
-    const makeGuessLambda = new NodejsFunction(this, 'MakeGuessFunction', {
-      runtime: lambda.Runtime.NODEJS_22_X,
-      entry: path.join(__dirname, "../lambda/makeGuess.ts"),
-      handler: 'handler',
-      environment: {
-        TABLE_NAME: dbTable.tableName,
-      },
-    });
+    const startGameLambda = new NodejsFunction(this, 'StartGameFunction', lambdaProps('startGame.ts'));
+    const makeGuessLambda = new NodejsFunction(this, 'MakeGuessFunction', lambdaProps('makeGuess.ts'));
 
     dbTable.grantReadWriteData(startGameLambda);
     dbTable.grantReadWriteData(makeGuessLambda);
